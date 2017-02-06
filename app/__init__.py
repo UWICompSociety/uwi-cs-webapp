19453ba7d346410231a539cb84361dfd0746b404
superrcoop <superrcoop@localhost.localdomain>
2017-02-06 18:25:52 -0500

    Added app.route template rendering and application responsiveness

--- app/__init__.py
+++ app/__init__.py
@@ -2,19 +2,19 @@
 Application module
 """
 
-from flask import Flask
+from flask import Flask, request, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import app_config
 
 db = SQLAlchemy()
 
+app = Flask(__name__, instance_relative_config=True)
 
 def create_app(config_name):
     """
     Todo
     """
-    app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile('config.py')
     db.init_app(app)
@@ -30,3 +30,26 @@ def create_app(config_name):
     app.register_blueprint(home_blueprint)
 
     return app
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/')
+def news():
+    return render_template('news.html')
+
+@app.route('/')
+def forum():
+    return render_template('forum.html')
+
+@app.route('/')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/')
+def stream():
+    return render_template('stream.html')
+
+
